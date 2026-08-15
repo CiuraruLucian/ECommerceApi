afc5e656d91af149bec022aad474b29421a203cf
Ciuraru Lucian <ciuraru.lucian8@yahoo.com>
2026-08-15 16:16:28 +0100

    feat: add UserController (admin user management) and finish Products CRUD

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -61,7 +61,7 @@ namespace ECommerceApi.Controllers
                 }
                
 
-                Product product = new Product { Name = dto.Name, Description = dto.Description, Price = dto.Price, Stock = dto.Stock };
+                Product product = new Product { Name = dto.Name, NormalizedName = NormalizedName, Description = dto.Description, Price = dto.Price, Stock = dto.Stock };
             
                 _context.Products.Add(product); 
 
@@ -82,15 +82,52 @@ namespace ECommerceApi.Controllers
         [Authorize(Roles = "Admin")]
 
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult>  Put(int id, [FromBody] ProductDto dto)
         {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            if (!string.IsNullOrWhiteSpace(dto.Name))
+            {
+                product.Name = dto.Name;
+                product.NormalizedName = dto.Name.Trim().ToLower();
+            }
+            if (!string.IsNullOrWhiteSpace(dto.Description))
+            {
+                product.Description = dto.Description;
+            }
+            if (!decimal.IsNegative(dto.Price))
+            {
+                product.Price = dto.Price;
+            }
+            if (!int.IsNegative(dto.Stock))
+            {
+                product.Stock = dto.Stock;
+            }
+            await _context.SaveChangesAsync();
+            
+            return Ok(new { product.Id, product.Name, product.Description, product.Price, product.Stock });
+
+
         }
 
         [Authorize(Roles = "Admin")]
 
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _context.Products.Remove(product);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Product deleted successfully." });
         }
     }
 }
---
+++ Models/UpdateUserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerceApi.Models
+{
+    public class UpdateUserDto
+    {
+        public string? Email { get; set; }
+
+        public string? UserName { get; set; }
+
+
+
+    }
+}
