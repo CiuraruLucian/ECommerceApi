0b2cd31b124989f771f6a6af9613b8e4c6389bf6
Lucian Ciuraru <ciuraru.lucian8@yahoo.com>
2026-08-27 16:43:59 +0100

    Added data annotations to AddCartItemDto for better validation

--- DTOs/AddCartItemDto.cs
+++ DTOs/AddCartItemDto.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.ComponentModel.DataAnnotations;
 
 namespace ECommerceApi.DTOs
 {
     public class AddCartItemDto
     {
+        [Required]
         public int ProductId { get; set; }
 
+        [Range(1, int.MaxValue)]
         public int Quantity { get; set; }
     }
 }
