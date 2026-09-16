468adbc4e7d0da78a5824cc2fb2029b1bf8c8ca1
Lucian Ciuraru <ciuraru.lucian8@yahoo.com>
2026-09-16 14:13:26 +0100

    Updated Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -79,8 +79,8 @@ builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = builder.Configuration["Redis:Connection"];
 });
-builder.Services.AddDbContext<AppDbContext>( options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddDbContext<AppDbContext>( options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<JwtService>();
 
 builder.Services.AddOpenApi();
